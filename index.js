77d6846cec6847762e603df3abe6597d18608877
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-02 18:14:08 +0100

    unction fibonacchi(n){     if ( n == 0 ) return 0;
        if (n == 1) return 1;
        return fibonacchi(n-2) + fibonacchi(n-1);
      }
      console.log(fibonacchi(3));

--- index.js
+++ index.js
@@ -182,4 +182,13 @@ for (let i = 0; i <= fuel; i += car2Fuel){
 console.log (distance);
 
 
+//фибоначи
+function fibonacchi(n){
+    if ( n == 0 ) return 0;
+   
+    if (n == 1) return 1;
+    return fibonacchi(n-2) + fibonacchi(n-1);
+  }
+  console.log(fibonacchi(3));
+
 
